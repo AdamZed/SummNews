340617fa9a3196697398994471ea57c0767ab834
michaelroudnitski <michaelroudnitski@gmail.com>
2018-09-23 09:07:38 -0400

    foundations for settings form

--- summnews/lib/settings/settings_tab.dart
+++ summnews/lib/settings/settings_tab.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
 
 class _SettingsWidgetState extends State<SettingsWidget> {
-
+  String techPreference = "noPref";
   @override
   Widget build(BuildContext context) {
-    return new Column(
-      children: <Widget>[
-        new Center(child: Text("Welcome to your settings")),
-        new Center(child: Text(":)")),
-      ]
-    );
+    return new Column(children: <Widget>[
+      new Text("Preferred Sources",
+          style: new TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+      new Row(children: <Widget>[
+        new Text("Tech", style: new TextStyle(fontSize: 18.0)),
+      ]),
+      new Row(children: <Widget>[
+        new Radio(
+            value: "noPref",
+            activeColor: Colors.red,
+            groupValue: techPreference,
+            onChanged: (String e) => setPreference),
+        new Text("No Preference")
+      ]),
+      new Row(children: <Widget>[
+        new Radio(
+            value: "bbc",
+            activeColor: Colors.red,
+            groupValue: techPreference,
+            onChanged: (String e) => setPreference),
+        new Text("BBC")
+      ])
+    ]);
+  }
+
+  void setPreference(String e) {
+    setState(() {
+      techPreference = e;
+    });
   }
 }
 
 class SettingsWidget extends StatefulWidget {
   @override
-  _SettingsWidgetState createState() => _SettingsWidgetState(); 
+  _SettingsWidgetState createState() => _SettingsWidgetState();
 }
